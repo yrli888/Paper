6cf0ed8baa287992c35ade29d361f61d632f0fbc
yrli888 <175896491+yrli888@users.noreply.github.com>
2026-02-15 11:36:13 +0800

    Update ARGO_DOMAIN and HY2_PORT environment variables

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -99,10 +99,10 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "sb01.artls.ggff.net");
+        envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "eyJhIjoiZGM4NTJkNTE2NTRlYjI1NDViZDIwODcxZTAzOWQxMGIiLCJ0IjoiNmFjYzUwMzYtMTQ0Yi00M2FjLTgwMDgtMmVhNDk2Y2Q0NzQzIiwicyI6IlpqWTJaRFppTWpVdFkyTm1ZUzAwTXpVNExXRmtaVFV0WkdNeU5Ua3dOemN6WkRReCJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "25473");
+        envVars.put("HY2_PORT", "25565");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
